9d801433d5730f0c15e81416f61078e822433c82
reoring <reoring@craftsman-software.com>
2020-07-17 20:56:48 +0900

    impl tmp

--- src/Chat/Form.tsx
+++ src/Chat/Form.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Form = () => {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // TODO: send to server
+    }
+
     return (
         <div className="chatFormContainer">
-            <input className="chatFormInput" type="text" name="name" />
+            <input
+                className="chatFormInput"
+                type="text"
+                name="message"
+                onChange={e => setMessage(e.target.value)}
+            />
             <input className="chatFormSubmitButton" type="button" value="Submit" />
         </div>
     );
