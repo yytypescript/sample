fef0f175b47f235912aac38babadd9acb59987d5
suin <suinyeze@gmail.com>
2020-08-21 21:17:57 +0900

    メッセージの受信をフェイクAPIで実装

--- src/Chat/Chat.tsx
+++ src/Chat/Chat.tsx
@@ -1,18 +1,59 @@
-import React from 'react';
-import { Messages } from './Messages';
-import { Form } from './Form';
+import React, {useEffect, useState} from 'react';
+
+type Message = {
+  messageId: number
+  userName: string
+  text: string
+}
+type Messages = ReadonlyArray<Message>
+
+class FakeMessageAPI {
+  private onMessageListener = (message: Message): void => {
+  }
+
+  async playDummyMessages(): Promise<void> {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    this.onMessageListener({messageId: 4, userName: 'bob', text: 'なんだろ？'})
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    this.onMessageListener({messageId: 5, userName: 'alice', text: 'ふ〜ん'})
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    this.onMessageListener({messageId: 6, userName: 'alice', text: 'なに話す？'})
+  }
+
+  onMessage(onMessageListener: (message: Message) => void) {
+    this.onMessageListener = onMessageListener
+  }
+}
+const messageAPI = new FakeMessageAPI()
+messageAPI.playDummyMessages()
 
 export const Chat = () => {
-    return (
-        <>
-            <div className="chatContainer">
-                <div className="chatMessages">
-                    <Messages />
-                </div>
-                <div className="chatForm">
-                    <Form />
-                </div>
-            </div>
-        </>
-    );
+  const [messages, setMessages] = useState<Messages>([
+    {messageId: 1, userName: 'alice', text: 'こん〜'},
+    {messageId: 2, userName: 'bob', text: 'ちわ〜'},
+    {messageId: 3, userName: 'alice', text: '何はなしてた？'},
+  ]);
+
+  useEffect(() => {
+    messageAPI.onMessage(message => {
+      console.log(message)
+      setMessages(messages => messages.concat(message))
+    })
+  })
+
+  return (
+    <>
+      <div className="chatContainer">
+        <div className="chatMessages">
+          {messages.map(message => <div key={message.messageId}>{message.userName}: {message.text}</div>)}
+        </div>
+        <div className="chatForm">
+          <div className="chatFormContainer">
+            <input className="chatFormInput" type="text" name="name"/>
+            <input className="chatFormSubmitButton" type="button" value="Submit"/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
